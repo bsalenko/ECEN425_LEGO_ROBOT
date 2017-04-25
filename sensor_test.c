dc24308f6d8fbc9069000d3e664fd10f6e3d09b9
bsalenko <bsalenko@gmail.com>
2017-04-25 18:05:52 +1200

    Add files via upload

--- sensor_test.c
+++ sensor_test.c
@@ -6,10 +6,13 @@
 #pragma config(Motor,  motorC,          leftM,         tmotorEV3_Large, PIDControl, driveLeft, encoder)
 #pragma config(Motor,  motorD,          liftM,         tmotorNXT, PIDControl, encoder)
 
+
+
 task main()
 {
 	int wallRange = 0;
 	int puckRange = 0;
+	int brightness = 0;
 
 	bFloatDuringInactiveMotorPWM  = false;
 
@@ -18,12 +21,14 @@ task main()
 	while(1){
   	wallRange = getUSDistance(wallS);
   	puckRange = getIRDistance(puckS);
+  	brightness = getColorAmbient(goalS);
 
-  	displayTextLine(0, "Wall: %d", wallRange);
-		displayTextLine(1, "Puck: %d", puckRange);
+  	displayTextLine(0, "Wall: %d", wallRange);//The smaller the value the closer you are, sometimes just to 255 for an unknown reason
+		displayTextLine(1, "Puck: %d", puckRange);//The smaller the value the closer you are
+		displayTextLine(2, "Brightness: %d", brightness);//30s when facing the ring, 1 when finger on the sensor, about 6 ambient
 
 		sleep(500);
-}
+	}
 
 
 }
